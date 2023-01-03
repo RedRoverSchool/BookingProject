74af5eac8ff11fe22c3f90bc1282151bfa4a5782
MaryMoroz <morozova.mary@gmail.com>
2023-01-03 18:33:18 -0500

    ref

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -7,6 +7,7 @@ describe('Verify Register account and login', () => {
         cy.get('div.inner a.login').click();
         cy.get('#byemail div.col-sm-6 > input[data-check="email"]').type(Cypress.env('login'), {force: true});
         cy.get('#byemail input[name="password"]').type(Cypress.env('password'), {force: true});
+        
         cy.get('#byemail input[value="SIGN IN"]').click();
     });
 });
