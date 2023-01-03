3dc7b326f23d1f1f01e2790c717c5ccdae4d57ff
MaryMoroz <morozova.mary@gmail.com>
2023-01-03 16:49:13 -0500

    ref test

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -5,8 +5,8 @@ describe('Verify Register account and login', () => {
     it('Verify text link "Register account now', () => {
         cy.visit("/");
         cy.get('div.inner a.login').click();
-        cy.get('#byemail div.col-sm-6 > input[data-check="email"]').type(Cypress.env('agent_login'), {force: true});
-        cy.get('#byemail input[name="password"]').type(Cypress.env('agent_password'), {force: true});
+        cy.get('#byemail div.col-sm-6 > input[data-check="email"]').type(Cypress.env('login'), {force: true});
+        cy.get('#byemail input[name="password"]').type(Cypress.env('password'), {force: true});
         cy.get('#byemail input[value="SIGN IN"]').click();
         // cy.get('div > a:nth-child(5)')
         // .should('be.visible')
