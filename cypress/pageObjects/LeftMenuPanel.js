d04a4acac5ab9c755faca01a0f46b9158540ab51
AlexMokeev <alexmokeevmd@gmail.com>
2023-01-20 17:30:02 -0500

    TC_03.03.01 Bookings management icon is visible

--- cypress/pageObjects/LeftMenuPanel.js
+++ cypress/pageObjects/LeftMenuPanel.js
@@ -1,6 +1,7 @@
 class LeftMenuPanel {
     elements = {
-        getMainElements: () => cy.get('.main-sidebar i')
+        getMainElements: () => cy.get('.main-sidebar i'),
+        getBookingManagement: () => cy.get('section > ul > li.active > a > i')
     }
 }
 export default LeftMenuPanel;
