2bba9a6353a35427710a291cc7d4b6733184339c
Artem Diuzhikov <a.diuzhikov@gmail.com>
2023-01-20 15:25:19 -0500

    add test numbers

--- cypress/e2e/testUiAndFunction/leftMenuPanel/menuPanelSection/us_03.01_MenuPanelMainElements.cy.js
+++ cypress/e2e/testUiAndFunction/leftMenuPanel/menuPanelSection/us_03.01_MenuPanelMainElements.cy.js
@@ -13,11 +13,11 @@ describe('US_03.01 Menu panel main elements', () => {
         cy.login(AGENT.email, AGENT.password);
     });
 
-    it('Verify the quantity of main elements (icons) in the menu', () => {
+    it('AT_03.01.01 | Verify the quantity of main elements (icons) in the menu', () => {
         leftMenuPanel.elements.getMainElements().should('have.length', 4);
     });
 
-    it('Verify the names of main elements in the menu', () => {
+    it('AT_03.01.02 | Verify the names of main elements in the menu', () => {
         leftMenuPanel.elements.getMainElementsNames().then(($mainElementsNames) => {
             const mainElementsNames = $mainElementsNames.toArray().map(el => el.innerText);
 
