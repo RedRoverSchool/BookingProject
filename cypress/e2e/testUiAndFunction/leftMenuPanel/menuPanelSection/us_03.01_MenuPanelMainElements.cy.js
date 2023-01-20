f6ac4d517ed2058cfa6902fb1485ecaaf767c8e4
Artem Diuzhikov <a.diuzhikov@gmail.com>
2023-01-20 15:13:55 -0500

    AT_03.01.02

--- cypress/e2e/testUiAndFunction/leftMenuPanel/menuPanelSection/us_03.01_MenuPanelMainElements.cy.js
+++ cypress/e2e/testUiAndFunction/leftMenuPanel/menuPanelSection/us_03.01_MenuPanelMainElements.cy.js
@@ -10,11 +10,18 @@ describe('US_03.01 Menu panel main elements', () => {
 
     beforeEach(function () {
         cy.visit('/');
+        cy.login(AGENT.email, AGENT.password);
     });
 
     it('Verify the quantity of main elements (icons) in the menu', () => {
-        cy.login(AGENT.email, AGENT.password);
-
         leftMenuPanel.elements.getMainElements().should('have.length', 4);
+    });
+
+    it('Verify the names of main elements in the menu', () => {
+        leftMenuPanel.elements.getMainElementsNames().then(($mainElementsNames) => {
+            const mainElementsNames = $mainElementsNames.toArray().map(el => el.innerText);
+
+            expect(mainElementsNames).to.deep.equal(['Booking', 'Bookings management', 'Account management', 'Contact us']);
+        });
     });
 });
