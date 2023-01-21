eac63e5877819b0dd2c645ac999008b2756f2e7a
AlexMokeev <alexmokeevmd@gmail.com>
2023-01-20 23:48:14 -0500

    fixed

--- cypress/pageObjects/LeftMenuPanel.js
+++ cypress/pageObjects/LeftMenuPanel.js
@@ -1,7 +1,7 @@
 class LeftMenuPanel {
     elements = {
         getMainElements: () => cy.get('.main-sidebar i'),
-        getBookingManagement: () => cy.get('section > ul > li.active > a > i')
+        getBookingManagement: () => cy.get('section>ul>li.active>a>i')
     }
 }
 export default LeftMenuPanel;
