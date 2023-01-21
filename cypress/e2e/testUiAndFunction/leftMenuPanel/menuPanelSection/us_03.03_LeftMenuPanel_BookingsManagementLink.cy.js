3df7b0a3e85e75180610aa7b0458946997724142
AlexMokeev <alexmokeevmd@gmail.com>
2023-01-20 19:32:37 -0500

    fixed again

--- cypress/e2e/testUiAndFunction/leftMenuPanel/menuPanelSection/us_03.03_LeftMenuPanel_BookingsManagementLink.cy.js
+++ cypress/e2e/testUiAndFunction/leftMenuPanel/menuPanelSection/us_03.03_LeftMenuPanel_BookingsManagementLink.cy.js
@@ -12,7 +12,7 @@ describe('US_03.03 Left Menu Panel > Bookings management link', () => {
         cy.visit('/');
     });
 
-    it('TC_03.03.01 Verify the "Bookings management" icon is visible', () => {
+    it('AT_03.03.01 Verify the "Bookings management" icon is visible', () => {
         cy.login(AGENT.email, AGENT.password);
 
         leftMenuPanel.elements.getBookingManagement().should('be.visible')
