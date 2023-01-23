980080bedcae2fbcd6c04b559b001181a9df3c43
AlexMokeev <alexmokeevmd@gmail.com>
2023-01-22 19:33:17 -0500

    AT_03.03.02 Verify text "Booking management"

--- cypress/e2e/testUiAndFunction/leftMenuPanel/menuPanelSection/us_03.03_LeftMenuPanel_BookingsManagementLink.cy.js
+++ cypress/e2e/testUiAndFunction/leftMenuPanel/menuPanelSection/us_03.03_LeftMenuPanel_BookingsManagementLink.cy.js
@@ -12,9 +12,15 @@ describe('US_03.03 Left Menu Panel > Bookings management link', () => {
         cy.visit('/');
     });
 
-    it('AT_03.03.01 Verify the "Bookings management" icon is visible', () => {
-        cy.login(AGENT.email, AGENT.password);
+        it('AT_03.03.01 Verify the "Bookings management" icon is visible', () => {
+            cy.login(AGENT.email, AGENT.password);
 
-        leftMenuPanel.getBookingManagement().should('be.visible')
-    });
+            leftMenuPanel.getBookingManagement().should('be.visible')
+        });
+
+        it('AT_03.03.02 Verify the Sidebar has text "Booking management" ', () => {
+            cy.login(AGENT.email, AGENT.password); 
+
+            leftMenuPanel.getBookingManagementNameLink().should('include.text', 'Bookings management')
+        });
 });
