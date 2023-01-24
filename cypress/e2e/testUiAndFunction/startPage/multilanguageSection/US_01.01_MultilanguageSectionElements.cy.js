ad8459bc60a6f775bba5b4bd8ce9467fadb04d9b
Natalia Kazakova <112223054+npkazakova@users.noreply.github.com>
2023-01-24 22:26:45 +0700

    Update US_01.01_MultilanguageSectionElements.cy.js

--- cypress/e2e/testUiAndFunction/startPage/multilanguageSection/US_01.01_MultilanguageSectionElements.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/multilanguageSection/US_01.01_MultilanguageSectionElements.cy.js
@@ -12,13 +12,13 @@ describe('US_01.01 | Multilanguage section elements UI and functionality', () =>
         cy.visit('/')
     });
 
-    it('AT_01.01.06 | Britain flag icon is clickable and changing language to English', function () {
+    it('AT_01.01.03 | Thailand flag icon is visible, function () {
         startPage
         .getThailandFlagIcon()
         .should('be.visible');
     }); 
     
-    it('AT_01.01.06 | Start page > Multilanguage section > Britain flag icon is clickable and changing language to English', function () {
+    it('AT_01.01.06 | Britain flag icon is clickable and changing language to English', function () {
         startPage.clickThailandFlagIcon();
         startPage.getLoginButton().should('include.text', this.buttons.thLoginBtnText);
         startPage.clickUKFlagIcon();
