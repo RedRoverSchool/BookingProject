f111d04cf31f95ed3241ad3800cddf3f02fd1469
Albina-star <akhalim1@student.ccp.edu>
2023-01-23 20:52:12 -0500

    empty line deleted

--- cypress/e2e/testUiAndFunction/header/envelopeIconSection/US_02.03_EnvelopeIconUIAndFunctionality.cy.js
+++ cypress/e2e/testUiAndFunction/header/envelopeIconSection/US_02.03_EnvelopeIconUIAndFunctionality.cy.js
@@ -4,15 +4,12 @@ import Header from "../../../../pageObjects/Header";
 
 const header = new Header();
 
-
 describe('US_02.03 Header Envelope icon UI and functionality', () => {
     const AGENT = Cypress.env('agent');
 
-    
     beforeEach(() => {
         cy.visit('/');
         cy.login(AGENT.email, AGENT.password);
-
     });
 
     it('AT_02.03.01 | Envelope icon/Helpdesk button is visible', () => {
