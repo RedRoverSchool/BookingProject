a358d242a99ffa2714342897103249b507ddb141
Albina-star <akhalim1@student.ccp.edu>
2023-01-26 00:55:37 -0500

    updated AT_02.03.02 with changed fixtures

--- cypress/e2e/testUiAndFunction/header/us_02.03_Header_EnvelopeIconUIandFunc.cy.js
+++ cypress/e2e/testUiAndFunction/header/us_02.03_Header_EnvelopeIconUIandFunc.cy.js
@@ -10,11 +10,8 @@ const AGENT = Cypress.env('agent');
 
 describe('US_02.03 Header Envelope icon UI and functionality', () => {
     beforeEach(function () {
-        cy.fixture('helpdeskPage/headers').then(header => {
-            this.header = header;
-         })
-         cy.fixture('helpdeskPage/filter').then(filter => {
-            this.filter = filter;
+        cy.fixture('helpdeskPage').then(helpdeskPage => {
+            this.helpdeskPage = helpdeskPage;
          })
         cy.visit('/');
         cy.login(AGENT.email, AGENT.password);
@@ -25,13 +22,9 @@ describe('US_02.03 Header Envelope icon UI and functionality', () => {
     })
 
     it('AT_02.03.02 | Verify Envelope icon is clickable and redirects to Helpdesk page; filter status is Active', function () {
-        header.clickEnvelopeIcon()
-
-        helpdeskPage.getHelpdeskHeader()
-        .should('include.text', this.header.mainHeaderPage ) 
-
-        helpdeskPage.getHelpdeskFilter()
-        .should('include.text', this.filter.selectFilter)
+        header.clickEnvelopeIcon();
+        helpdeskPage.getHelpdeskHeader().should('include.text', this.helpdeskPage.headers.mainHeaderPage);
+        helpdeskPage.getHelpdeskFilter().should('include.text', this.helpdeskPage.filter.selectFilterStatus);
    });
 })
 
