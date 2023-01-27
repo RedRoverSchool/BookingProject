9b4261108f16bba1aca82d3f0a94fb69281958b3
Yulia Lopatko <engineeryulia@gmail.com>
2023-01-27 12:34:06 -0700

    add preconditions

--- cypress/e2e/testUiAndFunction/header/US_02.05_UserDropdownMenuUIAndFunc.cy.js
+++ cypress/e2e/testUiAndFunction/header/US_02.05_UserDropdownMenuUIAndFunc.cy.js
@@ -19,27 +19,25 @@ describe('US_02.05 | User dropdown menu UI and functionality', () => {
 		})
 		cy.visit('/')
 		cy.login(AGENT.email, AGENT.password)
+		header.clickUserDropDownMenu()
 	});
 
 	it('AT_02.05.01 | Verify the User dropdown menu displays selection of 8 language icons', function () {
-		header.clickUserDropDownMenu()
 		header.getLanguageIcons()
 			.should('have.length', this.header.hyperLinks.languageIconQuantity)		
 	})
 	
 	it('AT_02.05.02 |Verify UK flag" icon is displayed', function () {
-		header.clickUserDropDownMenu()
+		
 		header.getFlagIconEn()
 			.should('be.visible')		
 	})
 
 	it('AT_02.05.04 |Verify UK flag  icon is clickable', function () {
-		header.clickUserDropDownMenu()
 		header.clickFlagIconTh()
 		createBookingPage.getPhoneNumberInputFild().should('have.attr', 'placeholder', this.createBookingPage.inputField.main_passenger.placeholderPhoneNumberTh)
 		header.clickUserDropDownMenu()
 		header.clickFlagIconEn()
 		createBookingPage.getPhoneNumberInputFild().should('have.attr', 'placeholder', this.createBookingPage.inputField.main_passenger.placeholderPhoneNumberEn)
-		
 	})
 });
