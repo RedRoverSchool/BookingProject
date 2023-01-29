9281db9e9a0be7fb797fcd443066e791d99f1508
Yulia Lopatko <engineeryulia@gmail.com>
2023-01-28 21:15:55 -0700

    update the test

--- cypress/e2e/testUiAndFunction/leftMenuPanel/us_03.02_Booking link.cy.js
+++ cypress/e2e/testUiAndFunction/leftMenuPanel/us_03.02_Booking link.cy.js
@@ -13,7 +13,7 @@ describe('US_03.02 Booking link', () => {
         cy.login(AGENT.email, AGENT.password);
     });
 
-    it('AT_03.02.01 | Verify that "Booking" icon in the left menu panel is visible', () => {
+    it('AT_03.02.01 |Verify that "Booking" icon in the left menu panel is visible', () => {
         leftMenuPanel.getBookingIcon().should('be.visible');
     });
 });
