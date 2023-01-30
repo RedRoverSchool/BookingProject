cf0af3e0b9ad006f548acd526b71b7345d8e157b
Mikhail Subbotin <msubbotin78@gmail.com>
2023-01-30 09:32:26 -0700

    AT_01.11.01

---
+++ cypress/e2e/testUiAndFunction/startPage/US_01.11_Header elements.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+import {StartPage} from "../../../pageObjects/StartPage";
+import {RegisterPopup} from "../../../pageObjects/StartPage";
+//import {RestorePopup} from "../../../pageObjects/StartPage";
+
+const startPage = new StartPage();
+const registerPopup = new RegisterPopup();
+//const restorePopup = new RestorePopup();
+
+describe('US_01.11 | Header elements', () => {
+
+    beforeEach(function () {
+		cy.visit('/');
+        startPage.clickRegisterAccountLink();
+	});
+
+    it('AT_01.11.01 | Verify the title `Register agent account` are visible in the header', () => {
+        registerPopup.getRegisterAgentAccountHeader().should('be.visible');
+    });
+
+});
