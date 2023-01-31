2c26b3f5bbdd6618c33d79ea331c99046c4ae322
Klavdia <kgold94@yahoo.com>
2023-01-31 11:59:08 -0800

    TC_05.01.01 Top section

--- cypress/pageObjects/BookingsListPage.js
+++ cypress/pageObjects/BookingsListPage.js
@@ -1,7 +1,16 @@
 class BookingsListPage {
+
     getBookingListHeader = () => cy.get('.page-title');
     getPrintButton = () => cy.get('.row .btn-print');
     getPrintButtonIcon = () => cy.get('.row .btn-print i');
+    getSearchField = () => cy.get('div #filterSearchQuery');
+    getSearchInput = () => cy.get('.pull-left input[placeholder="Search"]');
+    getBookingIdField = () => cy.get('div #filterTracker');
+    getStatusField = () => cy.get('.selection [placeholder="Status"]');
+    getRouteField = () => cy.get('div #select2-filterRoute-container');
+    getVehicleField = () => cy.get('div #select2-filterVehicle-container');
+    getClearLink = () => cy.get('div #filterClear');
+    getDateRange = () => cy.get('div #filterDateType');
 
     clickPrintButton() {
         this.clickPrintButton().click();
