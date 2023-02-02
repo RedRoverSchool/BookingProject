1fe1134a838f0aa535b77bc206a50cbbe90016f4
MaryMoroz <morozova.mary@gmail.com>
2023-02-02 15:36:38 -0500

    refactor

--- cypress/e2e/testActions/us_AC.05_CreateReservation.cy.js
+++ cypress/e2e/testActions/us_AC.05_CreateReservation.cy.js
@@ -10,13 +10,13 @@ const MANAGER = Cypress.env('manager');
 const AGENT = Cypress.env('agent');
 const CI = Cypress.env('CI');
 
-describe('US_AC.05 | Create reservation for 1 passenger', () => {  
+describe('US_AC.05 | Create reservation for 1 passenger', () => {
 
     before(function () {
         cy.cleanCiData(MANAGER.email, MANAGER.password, CI)
     })
 
-    before(function() {
+    before(function () {
         cy.visit('/')
         cy.login(AGENT.email, AGENT.password)
 
@@ -24,29 +24,31 @@ describe('US_AC.05 | Create reservation for 1 passenger', () => {
         createBookingPage.clickFridayButton();
 
         cy.intercept('/tools/**').as('getTrip')
-		cy.wait('@getTrip')
+        cy.wait('@getTrip')
 
         createBookingPage.clickFirstTripCard();
     });
 
-    beforeEach(function() {
+    beforeEach(function () {
         cy.fixture('createBookingPage').then(createBookingPage => {
-        this.createBookingPage = createBookingPage;
+            this.createBookingPage = createBookingPage;
         })
     });
 
     it('AT_AC.05.02| Create reservation for 1 passenger - Child', function () {
         createBookingPage.typeIntoMainPassengerNameField(this.createBookingPage.inputField.main_passenger.name);
-        createBookingPage.getMainPassengerFareTypeDropdownList().select('child', {force: true});
+        createBookingPage.getMainPassengerFareTypeDropdownList().select('child', { force: true });
         createBookingPage.clickReservationTicketArrow();
         createBookingPage.clickReservationTicketButton();
-        
-        cy.intercept('/tools/ping/**').as('getPopUp')
-        cy.wait('@getPopUp')
 
-        bookingPopup.getConfirmTicketButton().should('be.visible');
-        bookingPopup.getPassengerTitle().should('include.text', '(1)');
-        bookingPopup.getPassengersList().should('have.length', 1);
-        bookingPopup.getOnePassengerTypeLabel().should('have.text', 'Child:');   
+        cy.intercept('/tools/ping/**').as('getPopUp')
+        
+        cy.wait('@getPopUp').then(({ response }) => {
+            expect(response.statusCode).to.eq(200)
+            bookingPopup.getConfirmTicketButton().should('be.visible');
+            bookingPopup.getPassengerTitle().should('include.text', '(1)');
+            bookingPopup.getPassengersList().should('have.length', 1);
+            bookingPopup.getOnePassengerTypeLabel().should('have.text', 'Child:');
+        })
     });
 });   
