9cba10566a4bae0b4ba3996539e7ff7102a721df
Elvira Kh <vira2015@aol.com>
2023-02-05 22:24:39 -0500

    function formatDate added

--- cypress/e2e/testUiAndFunction/bookingsListPage/US_05.03_DateFilterUI.cy.js
+++ cypress/e2e/testUiAndFunction/bookingsListPage/US_05.03_DateFilterUI.cy.js
@@ -6,16 +6,22 @@ import LeftMenuPanel from "../../../pageObjects/LeftMenuPanel";
 const bookingsListPage = new BookingsListPage();
 const leftMenuPanel = new LeftMenuPanel();
 
+function formatDate(date) {
+    const optionsDate = { day: 'numeric', month: 'short', year: 'numeric' }
+    return date.toLocaleString('en-US', optionsDate)
+        .replace(/(\S{3}).(\d{1,2})(.).(\d{4})/, "$2 $1$3 $4")
+}
+
 describe('US_05.03 | Date filter UI', () => {
 
-    const AGENT = Cypress.env('agent');
+    const AGENT = Cypress.env('agent')
 
     before(() => {
         cy.visit('/');
         cy.login(AGENT.email, AGENT.password)
 
         //Precondition
-        leftMenuPanel.clickBookingManagementIcon();
+        leftMenuPanel.clickBookingManagementIcon()
     });
 
     beforeEach(function () {
@@ -27,16 +33,11 @@ describe('US_05.03 | Date filter UI', () => {
     it('AT_05.03.02 | Verify that the "calendar" dropdown has defaulted to the current date in format DD MMM, YYYY', function () {
         const today = new Date()
         const past7Date = new Date(new Date().setDate(today.getDate() - 6))
-        const optionsDate = { day: 'numeric', month: 'short', year: 'numeric' }
 
-        const todayFormat = today
-            .toLocaleString('en-US', optionsDate)
-            .replace(/(\S{3}).(\d{1,2})(.).(\d{4})/, "$2 $1$3 $4");
-        const past7DateFormat = past7Date
-            .toLocaleString('en-US', optionsDate)
-            .replace(/(\S{3}).(\d{1,2})(.).(\d{4})/, "$2 $1$3 $4");
-            
+        const todayFormatted = formatDate(today)
+        const past7DateFormatted = formatDate(past7Date)
+
         bookingsListPage.getFilterDefaultRange()
-            .should('have.text', `${past7DateFormat} - ${todayFormat}`)
+            .should('have.text', `${past7DateFormatted} - ${todayFormatted}`)
     })
 });
