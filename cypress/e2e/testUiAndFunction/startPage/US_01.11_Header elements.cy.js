bdb7affa1d8c30a241dcd64ded95795663227120
TimothyHuxx <dreamtim0101@gmail.com>
2023-02-06 19:45:03 -0800

    AT_01.11.04

--- cypress/e2e/testUiAndFunction/startPage/US_01.11_Header elements.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/US_01.11_Header elements.cy.js
@@ -21,4 +21,12 @@ describe('US_01.11 | Header elements', () => {
         registerPopup.getRegisterAgentAccountHeader().should('have.css','color', 'rgb(102, 102, 102)');
         registerPopup.getRegisterAgentAccountHeader().should('have.css','font-size', '30px');
     });
+
+    it.only('AT_01.11.04 | Verify that after clicking on the X button in the top right corner, Popup is closed and the main page appears', () => {
+        registerPopup.getRegisterModalPopup().should('have.attr', 'aria-hidden', 'false')
+        registerPopup
+            .clickRegisterPopupCloseButton()
+
+        registerPopup.getRegisterModalPopup().should('have.attr', 'aria-hidden', 'true')
+    });
 });
