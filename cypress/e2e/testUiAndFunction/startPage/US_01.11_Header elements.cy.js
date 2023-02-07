ff2b698055f9e9a626e9fe4d22c85253a6ad48c8
TimothyHuxx <dreamtim0101@gmail.com>
2023-02-06 20:35:11 -0800

    Update test name

--- cypress/e2e/testUiAndFunction/startPage/US_01.11_Header elements.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/US_01.11_Header elements.cy.js
@@ -22,11 +22,15 @@ describe('US_01.11 | Header elements', () => {
         registerPopup.getRegisterAgentAccountHeader().should('have.css','font-size', '30px');
     });
 
-    it.only('AT_01.11.04 | Verify that after clicking on the X button in the top right corner, Popup is closed and the main page appears', () => {
-        registerPopup.getRegisterModalPopup().should('have.attr', 'aria-hidden', 'false')
+    it('AT_01.11.04 | Verify the functionality of the close button', () => {
+        registerPopup
+            .getRegisterModalPopup()
+            .should('have.attr', 'aria-hidden', 'false')
         registerPopup
             .clickRegisterPopupCloseButton()
 
-        registerPopup.getRegisterModalPopup().should('have.attr', 'aria-hidden', 'true')
+        registerPopup
+            .getRegisterModalPopup()
+            .should('have.attr', 'aria-hidden', 'true')
     });
 });
