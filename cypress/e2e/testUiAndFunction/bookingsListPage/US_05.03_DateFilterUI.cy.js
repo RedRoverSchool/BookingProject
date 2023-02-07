cf6aad7972d9a685134a107ba4f7b8f7ed4374dd
Klavdia <kgold94@yahoo.com>
2023-02-06 19:23:42 -0800

    RF /formatting

--- cypress/e2e/testUiAndFunction/bookingsListPage/US_05.03_DateFilterUI.cy.js
+++ cypress/e2e/testUiAndFunction/bookingsListPage/US_05.03_DateFilterUI.cy.js
@@ -42,11 +42,11 @@ describe('US_05.03 | Date filter UI', () => {
 
     it('AT_05.03.05 | Verify that the filter date range dropdown has values', function () {
         bookingsListPage.clickDatesRangeList();
-        
+
         bookingsListPage.getDatesRangeListValues().then(options => {
             const dateRangesActual = options
-                                    .toArray()
-                                    .map(el =>el.innerHTML)
+                .toArray()
+                .map(el => el.innerHTML)
 
             expect(dateRangesActual).to.deep.equal(this.bookingsListPage.dropDown.datesRange);
         });
@@ -54,23 +54,23 @@ describe('US_05.03 | Date filter UI', () => {
     })
 
     it('AT_05.03.04 | Verify that the  DateType dropdown is visible and have 2 options', function () {
-        bookingsListPage.getDateRangeType() .should('be.visible');
-            bookingsListPage
-             .getDateRangeTypeDefault()
-             .should('have.text',
-        this.bookingsListPage.dropDown.datesRangeType.dateRangeTypeText);
-        
+        bookingsListPage.getDateRangeType().should('be.visible');
+        bookingsListPage
+            .getDateRangeTypeDefault()
+            .should('have.text',
+                this.bookingsListPage.dropDown.datesRangeType.dateRangeTypeText);
+
         bookingsListPage.clickDateRangeType();
         bookingsListPage.getDateRangeType()
             .should('have.length',
-        this. bookingsListPage.dropDown.datesRangeType.datesRangeTypeNumber)            
+                this.bookingsListPage.dropDown.datesRangeType.datesRangeTypeNumber)
         bookingsListPage.getDateRangeType().each(($el, i) => {
             cy.wrap($el)
                 .should('be.visible')
                 .and('have.text',
-        this.bookingsListPage.dropDown.datesRangeType.datesRangeTypeName[i]);
+                    this.bookingsListPage.dropDown.datesRangeType.datesRangeTypeName[i]);
         });
-    });   
+    });
 });
 
 
