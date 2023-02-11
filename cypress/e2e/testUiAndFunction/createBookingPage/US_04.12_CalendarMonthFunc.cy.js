e16ebbd6890756212cb52ed2e064b1882e80b30e
MARINA MONROE <mkarimullina@gmail.com>
2023-02-11 12:32:55 -0500

    skip_04/12

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.12_CalendarMonthFunc.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.12_CalendarMonthFunc.cy.js
@@ -4,7 +4,7 @@ import CreateBookingPage from "../../../pageObjects/CreateBookingPage";
 
 const createBookingPage = new CreateBookingPage();
 
-describe('US_04.12 | Calendar month functionality', () => {
+describe.skip('US_04.12 | Calendar month functionality', () => {
 	const AGENT = Cypress.env('agent');
 
 	before(function () {
