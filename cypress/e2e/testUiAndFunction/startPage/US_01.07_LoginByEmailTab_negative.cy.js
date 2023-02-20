e468a1ff237f9bd86877c4cf081e158e2ec33dda
AlexMokeev <alexmokeevmd@gmail.com>
2023-02-19 21:47:06 -0500

    RF | AT_01.07.02

--- cypress/e2e/testUiAndFunction/startPage/US_01.07_LoginByEmailTab_negative.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/US_01.07_LoginByEmailTab_negative.cy.js
@@ -32,16 +32,9 @@ describe('US_01.07 | Login by email tab negative', () => {
         });
 
     it.skip('AT_01.07.02 | Verify the error message after click Sign In with an invalid email', function () {
-        loginPopup
-            .getEmailInput()
-            .clear()
-            .type(this.startPage.dataInvalid.invalidEmail);
-        loginPopup
-            .getPasswordInput()
-            .clear()
-            .type(this.startPage.dataInvalid.validPassword);
-        loginPopup
-            .clickByEmailSignInButton();
+        loginPopup.enterEmail(this.startPage.dataInvalid.invalidEmail);
+        loginPopup.enterPassword(this.startPage.dataInvalid.validPassword);
+        loginPopup.clickByEmailSignInButton();
         loginPopup
             .getEmailErrorMessage()
             .should("be.visible")
