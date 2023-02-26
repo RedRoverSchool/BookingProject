a26b047b78f072b9262c457c1ae084b44cf6e41a
Natalia Kazakova <npkazakova@gmail.com>
2023-02-26 23:25:52 +0700

    AT_04.02.05

--- cypress/pageObjects/CreateBookingPage.js
+++ cypress/pageObjects/CreateBookingPage.js
@@ -3,7 +3,7 @@ class CreateBookingPage {
     getCreateBookingHeader = () => cy.get('div h1');
 
     //Departure/Arrival station
-    getDepartureStationSection = () => cy.get('.box-body.route-wrapper');
+    getDepartureStationSection = () => cy.get('.box-body.route-wrapper');    
     getDepartureStationDropdown = () => cy.get('#select2-departure-container');
     getListDepartureStation = () => cy.get('.select2-results .select2-results__option');
     getArrivalStationDropdown = () => cy.get('#select2-destination-container');
@@ -16,6 +16,7 @@ class CreateBookingPage {
     getLabelArrivalStation = () => cy.get('.destination-wrapper label')
 
     //Departure date
+    getDepartureDateSection = () => cy.get('.box-body.calendar-wrapper');
     getCalendarNextButton = () => cy.get('div .calendar-week-next');
     getCalendarPrevButton = () => cy.get('button.calendar-week-prev');
     getCalendarDays = () => cy.get('.calendar-day-selection-wrapper > .day-wrapper');
