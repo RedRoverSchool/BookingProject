b9071cfabe6d1b8191a90bf1fd11e3aa87770cfb
elviraKh1 <vira2015@aol.com>
2023-02-25 22:28:26 -0500

    rf js date functions

--- cypress/pageObjects/BookingsListPage.js
+++ cypress/pageObjects/BookingsListPage.js
@@ -79,7 +79,7 @@ class BookingsListPage {
     getDateFromCurrentDDMMMYYYY(days) {
         const date = this.getDate(days)
         const optionsDate = { day: 'numeric', month: 'short', year: 'numeric' }
-        return date.toLocaleString('en-US', optionsDate)
+        return date.toLocaleDateString('en-US', optionsDate)
             .replace(/(\S{3}).(\d{1,2})(.).(\d{4})/, "$2 $1$3 $4")
     }
     
@@ -88,17 +88,16 @@ class BookingsListPage {
     }
 
     getDateYYYYMMDD(date) {
-        const optionsDate = { day: 'numeric', month: 'numeric', year: 'numeric' }
-        optionsDate.month = optionsDate.day = '2-digit';
-        return date.toLocaleString('en-US', optionsDate)
-            .replace(/(\d{2}).(\d{2}).(\d{4})/, '$3-$1-$2')
+        const optionsDate = { day: '2-digit', month: '2-digit', year: 'numeric' };
+        return date.toLocaleDateString('en-US', optionsDate)
+                   .replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$3-$1-$2');
     }
 
     formatteddDatesRangeYYYYMMDD(dates) {
-        const arrayDatesRange = dates.split(' - ')
-        let day0 = this.getDateYYYYMMDD(new Date(arrayDatesRange[0]))
-        let day1 = this.getDateYYYYMMDD(new Date(arrayDatesRange[1]))
-        return `${day0} - ${day1}`
+        const [startDate, endDate] = dates.split(' - ');
+        const formattedStartDate = this.getDateYYYYMMDD(new Date(startDate));
+        const formattedEndDate = this.getDateYYYYMMDD(new Date(endDate));
+        return `${formattedStartDate} - ${formattedEndDate}`;
     }
 }
 
