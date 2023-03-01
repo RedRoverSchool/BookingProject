920911e8b3bf5cae77da7c6a0ff3f9ef25d8e5d4
08AlimDuiL <PortnovaLiudmila@yandex.ru>
2023-03-01 14:51:34 +0300

    Addd cy.session

--- cypress/e2e/testUiAndFunction/bookingsListPage/US_05.02_SearchSectionFunctionality.cy.js
+++ cypress/e2e/testUiAndFunction/bookingsListPage/US_05.02_SearchSectionFunctionality.cy.js
@@ -10,8 +10,8 @@ describe("US_05.02_Search section functionality", () => {
   const AGENT = Cypress.env("agent");
 
   before(() => {
+    cy.loginWithSession(AGENT.email, AGENT.password);
     cy.visit("/");
-    cy.login(AGENT.email, AGENT.password);
 
     //Precondition
     leftMenuPanel.clickBookingManagementIcon();
