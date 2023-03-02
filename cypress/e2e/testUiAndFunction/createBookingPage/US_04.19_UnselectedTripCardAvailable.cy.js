aa0e2f0abba6ac213e5af9f43f62be8018dec601
yuro4ka250686 <selivanovyurik@gmail.com>
2023-03-02 16:22:09 +0300

    add AT_04.19.03

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.19_UnselectedTripCardAvailable.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.19_UnselectedTripCardAvailable.cy.js
@@ -19,7 +19,11 @@ describe('US_04.19 | Unselected trip card available UI', function() {
     beforeEach(function () {
         cy.fixture('colors').then(colors => {
             this.colors = colors;
-        }) 
+        });
+        
+        cy.fixture('createBookingPage').then(createBookingPage => {
+            this.createBookingPage = createBookingPage;
+        });
     });
 
     it('AT_04.19.01 | Verify Trips card with "Number available tickets" label is visible as unselected', function() {
@@ -32,6 +36,18 @@ describe('US_04.19 | Unselected trip card available UI', function() {
     it('AT_04.19.02 | Verify Background color of Trip cards is white (#FFF)', function() {
         createBookingPage.getDepartureTripCardsList().each($el => {
             cy.wrap($el).should('have.css', 'lighting-color', this.colors.white)  
+        })
+    });
+
+    it('AT_04.19.03 | Verify "Class" icon is visible as the icon of available vehicle (bus or ferry) and displayed on the left-top corner', function() {
+        createBookingPage.getClassUnselectedTripCards().each($el => {
+            let text = $el.text().match(/\D/g).join('').trim()
+            let allClasses = `${this.createBookingPage.seatsTableTitle[0]} ${this.createBookingPage.seatsTableTitle[1]} ${this.createBookingPage.seatsTableTitle[2]}`
+
+            cy.wrap($el).should('be.visible')
+                        .and('have.css', 'color', this.colors.darkGray)
+
+            expect(allClasses).to.contain(text)
         })
     });
 });
