fb6a112417ad0aa62e1cc4ff9313ed55becca910
SvetlanaKanel <svetlana.kanel@gmail.come>
2023-03-04 23:11:52 -0500

    RF | AT_04.18.01 add intercept and wait

--- cypress/e2e/testUiAndFunction/createBookingPage/US_04.18_SelectedTripCard.cy.js
+++ cypress/e2e/testUiAndFunction/createBookingPage/US_04.18_SelectedTripCard.cy.js
@@ -1,6 +1,6 @@
 /// <reference types ="cypress" />
 import CreateBookingPage from "../../../pageObjects/CreateBookingPage";
-import waitForToolsPing from "../../../support/utilities/waitForToolsPing";
+//import waitForToolsPing from "../../../support/utilities/waitForToolsPing";
 
 const createBookingPage = new CreateBookingPage();
 
@@ -8,11 +8,12 @@ describe('US_04.18 | Create booking page > Selected trip card UI', () => {
     const AGENT = Cypress.env('agent');
 
     before(() => {
+        cy.intercept('/tools/ping/**').as('getToolsPing');
         cy.loginWithSession(AGENT.email, AGENT.password);
         cy.visit('/');
-        createBookingPage.clickCalendarNextButton();
-        waitForToolsPing();
-        createBookingPage.clickSecondTripCard();
+        createBookingPage.clickCalendarNextButton(); 
+        cy.wait('@getToolsPing');     
+        createBookingPage.clickSecondTripCard();      
     })
     beforeEach(function () {
         cy.fixture('colors').then(colors => {
