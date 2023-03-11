031c0fd1485f9b9c86b2e156972978a649fe70b1
AlexMokeev <alexmokeevmd@gmail.com>
2023-03-11 13:59:53 -0500

    Delete "only"

--- cypress/e2e/testUiAndFunction/startPage/US_01.18_RestorePasswordPopupFooterUIAndFunc.cy.js
+++ cypress/e2e/testUiAndFunction/startPage/US_01.18_RestorePasswordPopupFooterUIAndFunc.cy.js
@@ -44,7 +44,7 @@ describe ('US 01.18 | Restore password Popup > Footer UI and functionality', { t
             .and('have.css', 'font-size', this.startPage.inputField.registerPopup.front_size)
     });
 
-    it.only('AT_01.18.04 | Verify the link “login” has rgb(66, 139, 202) color and 14px font-size', function () {
+    it('AT_01.18.04 | Verify the link “login” has rgb(66, 139, 202) color and 14px font-size', function () {
         restorePopup
         .getLoginLink()
         .should('have.css','color', this.colors.blue)
