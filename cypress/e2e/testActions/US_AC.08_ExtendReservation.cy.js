9a04f00cb75dceaa41c0674c2d42f8d67e907824
SvetlanaKanel <svetlana.kanel@gmail.come>
2023-03-12 15:52:59 -0400

    Fixture joining

--- cypress/e2e/testActions/US_AC.08_ExtendReservation.cy.js
+++ cypress/e2e/testActions/US_AC.08_ExtendReservation.cy.js
@@ -8,23 +8,22 @@ const AGENT = Cypress.env('agent')
 
 describe('US_AC.08 | ACTIONS > Extend reservation', { tags: ['regression'] }, function () {
     beforeEach(function () {
-        cy.fixture('createBookingPage').then(createBookingPage => {
-            this.createBookingPage = createBookingPage;
+        cy.fixture('createBookingPage').then(bookingData  => {
+            this.bookingData  = bookingData ;
         })
         cy.fixture('bookingPopup').then(bookingPopUpData => {
             this.bookingPopUpData = bookingPopUpData;
         })
-    })
-    beforeEach(function () {
+
         cy.cleanData();
         cy.loginWithSession(AGENT.email, AGENT.password);
         cy.visit('/');
     })
-
+   
     it('AT_AC.08.01 | Extend one time and verify the time counter in the text field "This reservation will expire in" is updated and starts counting down from 00:15:00', function () {
         let numberOfPassengers = 1;
-        let passengerName = this.createBookingPage.inputField.main_passenger.name;
-        let passengerFareTypes = this.createBookingPage.dropdowns.fareType.fareTypesNames[0];
+        let passengerName = this.bookingData.inputField.main_passenger.name;
+        let passengerFareTypes = this.bookingData.dropdowns.fareType.fareTypesNames[0];
         createBookingPage.createReservationSecondTrip(numberOfPassengers, passengerName, passengerFareTypes);
 
         bookingPopup.clickBtnExtend();
--- cypress/pageObjects/BookingPopup.js
+++ cypress/pageObjects/BookingPopup.js
@@ -26,7 +26,7 @@ class BookingPopup {
     }
 
     clickBtnExtend() {
-
+        this.getBtnExtend().click();
     }
 
     getTimeFromTimer() {        
