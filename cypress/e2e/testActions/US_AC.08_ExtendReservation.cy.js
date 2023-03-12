afa2ec4da7860b27d3e55ebdfd66a9e1bdd49b16
SvetlanaKanel <svetlana.kanel@gmail.come>
2023-03-12 13:52:47 -0400

    fixture chenging

--- cypress/e2e/testActions/US_AC.08_ExtendReservation.cy.js
+++ cypress/e2e/testActions/US_AC.08_ExtendReservation.cy.js
@@ -24,7 +24,7 @@ describe('US_AC.08 | ACTIONS > Extend reservation', { tags: ['regression'] }, fu
     it('AT_AC.08.01 | Extend one time and verify the time counter in the text field "This reservation will expire in" is updated and starts counting down from 00:15:00', function () {
         let numberOfPassengers = 1;
         let passengerName = this.createBookingPage.inputField.main_passenger.name;
-        let passengerFareTypes = this.createBookingPage.fareTypeDropdown[0];
+        let passengerFareTypes = this.createBookingPage.dropdowns.fareType.fareTypesNames[0];
         createBookingPage.createReservationSecondTrip(numberOfPassengers, passengerName, passengerFareTypes);
 
         bookingPopup.clickBtnExtend();
